b7106a3421eab9316366699912f0b8a7d79d041e
Lilian Deceuninck--Cappelaere <deceuninck.lilian1@gmail.com>
2025-01-13 10:41:35 +0100

    Create test.c

---
+++ test.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+const char *filename = "présentation.txt";
+
+int main(int argc, char *argv[])
+{
+    // open the file
+    FILE *in_file = fopen(filename, "r");
+
+    // read the file size
+    struct stat sb;
+    stat(filename, &sb);
+
+    // allocation of the character array
+    char *file_contents = malloc(sb.st_size);
+
+    // display line by line
+    while (fscanf(in_file, " %[^\n] ", file_contents) != EOF)
+    {
+        printf("> %s\n", file_contents);
+    }
+
+    // close the file
+    fclose(in_file);
+    exit(EXIT_SUCCESS);
+}
