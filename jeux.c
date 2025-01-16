4f944ccbebdacbf374f89ec39fc28ab8c96f78e9
Lilian Deceuninck--Cappelaere <deceuninck.lilian1@gmail.com>
2025-01-16 19:57:55 +0100

    Update jeux.c

--- jeux.c
+++ jeux.c
@@ -447,9 +447,12 @@ int main(int argc, char *argv[])
 
     snprintf(fileName, 13, "%s/chap1.txt", language);
     chap1(fileName, filestats, language, &player, &zombie);
+
     snprintf(fileName, 13, "%s/chap2.txt", language);
     chap2(fileName, filestats, language, &player, &zombie);
-    
-    
+
+    // snprintf(fileName, 13, "%s/chap3.txt", language);
+    // chap3(fileName, filestats, language, &player, &zombie);
+
     return 0;
 }
